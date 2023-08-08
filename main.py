987dfa1090c397b281b5ac29a1a6146c084723f0
Marwan Mohammed <116826792+Rookie-kinda@users.noreply.github.com>
2023-08-08 12:42:46 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from email_sender import confirmation_mail
+def main():
+    if __name__ == "__main__":
+        attachment_file = "attachment.extention"
+        body = 
+      """
+      Add body of Email here
+      """
+        
+        confirmation_mail("sender@gmail.com", "reciever@something.com", "password", "Subject", body, "attachment")
+
+main()
